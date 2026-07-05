53261665756c107c0d8927bb121c5e4e52450241
Luiz <luizfilipeholiday@gmail.com>
2026-07-05 04:51:08 -0300

    chore: port CSS verbatim into the React client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,10 @@
+import "./styles/tokens.css";
+import "./styles/app.css";
+import "./styles/rig-sheet.css";
+import "./styles/battle.css";
+import "./styles/join.css";
+import "./styles/glossary.css";
+import "./styles/rig-wizard.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
