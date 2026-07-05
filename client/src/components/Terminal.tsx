26e22d089c582d030130551c501e62daf31a199d
Luiz <luizfilipeholiday@gmail.com>
2026-07-05 05:37:24 -0300

    feat: OutcomeBanner

--- client/src/components/Terminal.tsx
+++ client/src/components/Terminal.tsx
@@ -1,11 +1,13 @@
 import { Topbar } from "./Topbar";
 import { Stage } from "./Stage";
+import { OutcomeBanner } from "./OutcomeBanner";
 
 export function Terminal() {
   return (
     <div className="term">
       <Topbar />
       <Stage />
+      <OutcomeBanner />
       {/* ChatFab + ChatPanel mount here (Task 34) */}
     </div>
   );
