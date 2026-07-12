512548f5218d00ee836f5a679e3df64d42fa7f10
Luiz <luizfilipeholiday@gmail.com>
2026-07-12 00:36:22 -0300

    feat(v2): component-row part labels are click-to-explain

--- client/src/v2/components/CompRow.test.tsx
+++ client/src/v2/components/CompRow.test.tsx
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { expect, test, vi } from "vitest";
+import { V2GlossaryTipProvider } from "../state/V2GlossaryTipContext";
 import { CompRow } from "./CompRow";
+
+function wrap(node: React.ReactNode) {
+  return render(<V2GlossaryTipProvider>{node}</V2GlossaryTipProvider>);
+}
 
 test("damage and repair dispatch the right commands", async () => {
   const user = userEvent.setup();
   const onCommand = vi.fn();
-  render(<CompRow rigName="STALKER" loc="hull" comp={{ sp: 4, max: 6, destroyed: false }} onCommand={onCommand} />);
+  wrap(<CompRow rigName="STALKER" loc="hull" comp={{ sp: 4, max: 6, destroyed: false }} onCommand={onCommand} />);
   await user.click(screen.getByRole("button", { name: /Damage hull/i }));
   expect(onCommand).toHaveBeenCalledWith("damage", { name: "STALKER", loc: "hull", amount: "1" });
   await user.click(screen.getByRole("button", { name: /Repair hull/i }));
@@ -14,6 +19,6 @@ test("damage and repair dispatch the right commands", async () => {
 });
 
 test("shows CATASTROPHIC at 0 SP", () => {
-  render(<CompRow rigName="X" loc="legs" comp={{ sp: 0, max: 5, destroyed: false }} onCommand={vi.fn()} />);
+  wrap(<CompRow rigName="X" loc="legs" comp={{ sp: 0, max: 5, destroyed: false }} onCommand={vi.fn()} />);
   expect(screen.getByText("CATASTROPHIC")).toBeInTheDocument();
 });
